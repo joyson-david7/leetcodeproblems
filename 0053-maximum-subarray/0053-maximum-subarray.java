0e6d95f595b95974f2cf30100166b5db8b698cab
joyson-david7 <joyhayati710@gmail.com>
2026-07-28 09:39:12 +0530

    Time: 1 ms (99.95%), Space: 77.3 MB (48.89%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int current=0;
+        int maxi=nums[0];
+        for(int i=0;i<nums.length;i++){
+            current+=nums[i];
+            maxi=Math.max(maxi,current);
+            if(current<0)
+            current=0;
+        }
+        return maxi;
+    }
+}
